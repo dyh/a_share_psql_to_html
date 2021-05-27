39c1a272009d7d743c574ac16863c1e337433f62
dyh <1358366+dyh@users.noreply.github.com>
2021-05-27 09:17:17 +0800

    daily

---
+++ predict_result_to_html.py
@@ -0,0 +1,100 @@
+import time
+
+import config
+from utils.datetime import get_week_day
+from utils.psqldb import Psqldb
+
+if __name__ == '__main__':
+    # 获取要输出到html的tic
+    config.SINGLE_A_STOCK_CODE = ['sh.600036', ]
+
+    # 从 index.html.template 文件中 读取 网页模板
+    with open('./template/index.html.template', 'r') as index_page_template:
+        text_index_page_template = index_page_template.read()
+
+        all_text_card = ''
+
+        # 从 card_predict_result.template 文件中 读取 结果卡片 模板
+        with open('./template/card_predict_result.template', 'r') as result_card_template:
+            text_card = result_card_template.read()
+
+            # psql对象
+            psql_object = Psqldb(database=config.PSQL_DATABASE, user=config.PSQL_USER,
+                                 password=config.PSQL_PASSWORD, host=config.PSQL_HOST, port=config.PSQL_PORT)
+
+            for tic in config.SINGLE_A_STOCK_CODE:
+                # 复制一个结果卡片模板，将以下信息 填充到卡片中
+                copy_text_card = text_card
+
+                # 获取数据库中最大日期
+                sql_cmd = f'SELECT "date" FROM "public"."{tic}" ORDER BY agent, vali_period_value ASC LIMIT 1'
+                max_date = psql_object.fetchone(sql_cmd)
+                max_date = str(max_date[0])
+
+                # 用此最大日期查询出一批数据
+                sql_cmd = f'SELECT "id", "agent", "vali_period_value", "pred_period_name", "action", "hold", "day" ' \
+                          f'FROM "public"."{tic}" WHERE "date" = \'{max_date}\' ORDER BY agent, vali_period_value ASC'
+
+                list_result = psql_object.fetchall(sql_cmd)
+
+                text_table_tr_td = ''
+
+                action_list = []
+                hold_list = []
+
+                for item_result in list_result:
+                    # 替换字符串内容
+                    copy_text_card = copy_text_card.replace('<%tic%>', tic)
+                    id1, agent1, vali_period_value1, pred_period_name1, action1, hold1, day1 = item_result
+
+                    action_list.append(action1)
+                    hold_list.append(hold1)
+
+                    text_table_tr_td += f'<tr><td>{id1}</td><td>{agent1}</td><td>{vali_period_value1}</td>' \
+                                        f'<td>{pred_period_name1}</td><td>{action1}</td>' \
+                                        f'<td>{hold1}</td><td>{day1}</td></tr>'
+                    pass
+                pass
+
+                # 日期
+                date1 = max_date + ' ' + get_week_day(max_date)
+                copy_text_card = copy_text_card.replace('<%date%>', date1)
+
+                # 按 hold 分组，选出数量最多的 hold
+                sql_cmd = f'SELECT "hold", COUNT("id") as count1 ' \
+                          f'FROM "public"."{tic}" WHERE "date" = \'{max_date}\' GROUP BY "hold"' \
+                          f' ORDER BY count1 DESC LIMIT 1'
+
+                max_hold = psql_object.fetchone(sql_cmd)[0]
+                copy_text_card = copy_text_card.replace('<%most_hold%>', str(max_hold))
+
+                # 按 action 分组，取数量最多的 action
+                sql_cmd = f'SELECT "action", COUNT("id") as count1 ' \
+                          f'FROM "public"."{tic}" WHERE "date" = \'{max_date}\' GROUP BY "action"' \
+                          f' ORDER BY count1 DESC LIMIT 1'
+
+                max_action = psql_object.fetchone(sql_cmd)[0]
+                copy_text_card = copy_text_card.replace('<%most_action%>', str(max_action))
+
+                # 表格
+                all_text_card += copy_text_card.replace('<%predict_result_table_tr_td%>', text_table_tr_td)
+                all_text_card += '\r\n'
+            pass
+            psql_object.close()
+        pass
+
+        # 将多个 卡片模板 替换到 网页模板
+        text_index_page_template = text_index_page_template.replace('<%page_content%>', all_text_card)
+
+        current_time_point = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+        text_index_page_template = text_index_page_template.replace('<%page_time_point%>', current_time_point)
+
+        text_index_page_template = text_index_page_template.replace('<%page_title%>', 'A股预测')
+
+        # 写入网页文件
+        with open('./index.html', 'w') as file_index:
+            file_index.write(text_index_page_template)
+            pass
+        pass
+
+    pass
---
+++ utils/datetime.py
@@ -0,0 +1,177 @@
+import datetime
+import time
+
+
+def time_point(time_format='%Y%m%d_%H%M%S'):
+    return time.strftime(time_format, time.localtime())
+
+
+def get_datetime_from_date_str(str_date):
+    """
+    由日期字符串获得datetime对象
+    :param str_date: 日期字符串，格式 2021-10-20
+    :return: datetime对象
+    """
+    year_temp = str_date.split('-')[0]
+    month_temp = str_date.split('-')[1]
+    day_temp = str_date.split('-')[2]
+    return datetime.date(int(year_temp), int(month_temp), int(day_temp))
+
+
+def get_next_work_day(datetime_date, next_flag=1):
+    """
+    获取下一个工作日
+    :param datetime_date: 日期类型
+    :param next_flag: -1 前一天，+1 后一天
+    :return: datetime.date
+    """
+    if next_flag > 0:
+        next_date = datetime_date + datetime.timedelta(
+            days=7 - datetime_date.weekday() if datetime_date.weekday() > 3 else 1)
+    else:
+        # 周二至周六
+        if 0 < datetime_date.weekday() < 6:
+            next_date = datetime_date - datetime.timedelta(days=1)
+            pass
+        elif 0 == datetime_date.weekday():
+            # 周一
+            next_date = datetime_date - datetime.timedelta(days=3)
+        else:
+            # 6 == datetime_date.weekday():
+            # 周日
+            next_date = datetime_date - datetime.timedelta(days=2)
+            pass
+        pass
+    return next_date
+
+
+def get_next_day(datetime_date, next_flag=1):
+    """
+    获取下一个日期
+    :param datetime_date: 日期类型
+    :param next_flag: -2 前2天，+1 后1天
+    :return: datetime.date
+    """
+    if next_flag > 0:
+        next_date = datetime_date + datetime.timedelta(days=abs(next_flag))
+    else:
+        next_date = datetime_date - datetime.timedelta(days=abs(next_flag))
+    return next_date
+
+
+def get_today_date():
+    # 获取今天日期
+    time_format = '%Y-%m-%d'
+    return time.strftime(time_format, time.localtime())
+
+
+def is_greater(date1, date2):
+    """
+    日期1是否大于日期2
+    :param date1: 日期1字符串 2001-03-01
+    :param date2: 日期2字符串 2001-01-01
+    :return: True/Flase
+    """
+    temp1 = time.strptime(date1, '%Y-%m-%d')
+    temp2 = time.strptime(date2, '%Y-%m-%d')
+
+    if temp1 > temp2:
+        return True
+    else:
+        return False
+    pass
+
+
+def get_begin_vali_date_list(end_vali_date):
+    """
+    获取7个日期列表
+    :return: list()
+    """
+
+    list_result = list()
+
+    # 20周期
+    begin_vali_date = get_next_day(end_vali_date, next_flag=-28)
+    list_result.append((20, begin_vali_date))
+
+    # 30周期
+    begin_vali_date = get_next_day(end_vali_date, next_flag=-42)
+    list_result.append((30, begin_vali_date))
+
+    # 40周期
+    begin_vali_date = get_next_day(end_vali_date, next_flag=-56)
+    list_result.append((40, begin_vali_date))
+
+    # 50周期
+    begin_vali_date = get_next_day(end_vali_date, next_flag=-77)
+    list_result.append((50, begin_vali_date))
+
+    # 60周期
+    begin_vali_date = get_next_day(end_vali_date, next_flag=-91)
+    list_result.append((60, begin_vali_date))
+
+    # 72周期
+    begin_vali_date = get_next_day(end_vali_date, next_flag=-108)
+    list_result.append((72, begin_vali_date))
+
+    # 90周期
+    begin_vali_date = get_next_day(end_vali_date, next_flag=-134)
+    list_result.append((90, begin_vali_date))
+
+    return list_result
+
+
+def get_end_vali_date_list(begin_vali_date):
+    """
+    获取7个日期列表
+    :return: list()
+    """
+
+    list_result = list()
+
+    # 20周期
+    end_vali_date = get_next_day(begin_vali_date, next_flag=28)
+    list_result.append((20, end_vali_date))
+
+    # 30周期
+    end_vali_date = get_next_day(begin_vali_date, next_flag=42)
+    list_result.append((30, end_vali_date))
+
+    # 40周期
+    end_vali_date = get_next_day(begin_vali_date, next_flag=56)
+    list_result.append((40, end_vali_date))
+
+    # 50周期
+    end_vali_date = get_next_day(begin_vali_date, next_flag=77)
+    list_result.append((50, end_vali_date))
+
+    # 60周期
+    end_vali_date = get_next_day(begin_vali_date, next_flag=91)
+    list_result.append((60, end_vali_date))
+
+    # 72周期
+    end_vali_date = get_next_day(begin_vali_date, next_flag=108)
+    list_result.append((72, end_vali_date))
+
+    # 90周期
+    end_vali_date = get_next_day(begin_vali_date, next_flag=134)
+    list_result.append((90, end_vali_date))
+
+    return list_result
+
+
+def get_week_day(string_time_point):
+    week_day_dict = {
+        0: '周一',
+        1: '周二',
+        2: '周三',
+        3: '周四',
+        4: '周五',
+        5: '周六',
+        6: '周日',
+    }
+
+    day_of_week = datetime.datetime.fromtimestamp(
+        time.mktime(time.strptime(string_time_point, "%Y-%m-%d"))).weekday()
+
+    return week_day_dict[day_of_week]
